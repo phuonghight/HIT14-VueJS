77682231e51220eb21ef0e893683e11c50d607f1
= <trandanhkhang482004@gamil.com>
2024-03-23 00:13:31 +0700

    change

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # HIT 14 VueJS Learning Group
 
 Nhóm học tập và trao đổi kiến thức về VueJS của Câu Lạc Bộ Tin Học HIT khóa 14
+
+test
--- lesson_02/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-// import App from './App.vue'
-import Exercise from './Exercise.vue'
-
-createApp(Exercise).mount('#app')
-// createApp(App).mount('#app')
