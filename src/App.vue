ef6ddd7e1fd3f1b841dd8816ac3292d3aef62aff
= <trandanhkhang482004@gamil.com>
2024-03-23 14:57:22 +0700

    khang update lessoon02

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,61 @@
 <script setup>
-import { ref, reactive, computed } from "vue";
 
-const msg = "Hellow World!";
-const h1Class = "h111111111111111";
-const url = "https://fb.com";
-function abc() {
-  return 1;
-}
-const classOrId = "id";
-
-const onSubmit = () => {
-  alert("Submit form");
-};
+// import HelloWorld from './components/HelloWorld.vue'
+import {reactive, ref, computed} from 'vue';
 
 const count = ref(0);
 const increase = () => {
   count.value += 1;
-  console.log(count);
-  console.log(count.value);
-};
+}
 
 const isDarkMode = ref(false);
 
-const userReative = reactive({
-  name: "",
-  age: 0,
-});
-Object.assign(userReative, { name: "Phuong", height: 1.73 });
+const user = reactive({
+  name: "Khang",
+  age: '19'
+}) 
+Object.assign(user, {height: 1.5});
 
-// const userRef = ref({
-//   name: "Phuong",
-//   age: 21,
-// });
+const increaseAge = () => {
+  user.age++;
+}
 
-// userRef.value = {};
+const msg = "Hello VueJS";
+const h1Class = "h1";
+const url = "youtube.com";
+const classOrId = "class";
+function onSubmit() {
+  alert('Hello VueJS');
+}
 
 const isEven = computed(() => {
-  console.log("Recomputed");
+  isDarkMode.value = false;
   return count.value % 2 === 0;
-});
+})
 
-const arr = ref([1, 2, 3, 4, 5, 6]);
-const pushARandomNumber = () => {
-  arr.value.push(~~(Math.random() * 10));
-};
+let arr = ref([1,2,3]);
+
 const total = computed(() => {
   return arr.value.reduce((pre, cur) => pre + cur, 0);
 });
 
-total.value = 26;
-console.log(total.value);
+const addArr = () => {
+  let num = ~~(Math.random()*10);
+  arr.value.push(num);
+  console.log(arr);
+}
+
+const removeArr = () => {
+  let num = Math.random() * 10;
+  num = Math.round(num)
+  arr.value.slice(arr.value.length, 1);
+  console.log(arr);
+}
 
 // Class and Style Binding
 const active = ref(false);
-const color = ref("red");
-const objClass = reactive({ "is-active": active, isisisActive: active });
-const styleObj = reactive({ "font-size": "12px", color: "orange" });
+// const objClass = reactive({'is-active': active});
+
 </script>
 
 <template>
@@ -66,42 +66,47 @@ const styleObj = reactive({ "font-size": "12px", color: "orange" });
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
-    <h1
-      class="h1"
-      :class="{ 'is-active': active, isisisActive: active }"
-      :style="{ 'font-size': total + 'px', color: 'orange' }"
-    >
-      {{ msg }}
-    </h1>
-    <!-- <h1 class="h1" :class="[active ? 'is-active' : 'no-active']">
-      {{ msg }}
-    </h1> -->
-    <button @click="active = !active">Active</button>
-    <!-- <a :href="url" target="_blank">{{ url }}</a>
-    <h2 :[classOrId]="classOrId">{{ true }}</h2> -->
-
-    <!-- <form @submit.prevent="onSubmit">
-      <input type="number" />
-      <button>Submit</button>
-    </form> -->
-
-    <!-- <h2>Count: {{ count }}</h2>
-    <p>Count is Even: {{ isEven }}</p>
-    <button @click="increase">increase</button>
-
-    <p>Dark Mode: {{ isDarkMode }}</p>
-    <button @click="isDarkMode = !isDarkMode">
-      {{ isDarkMode ? "Light" : "Dark" }}
-    </button> -->
-
-    <!-- <h1>Name: {{ userReative }}</h1> -->
-    <!-- <h1>Name: {{ user.name }}</h1>
-    <h2>Age: {{ user.age }}</h2> -->
-
-    <h2>{{ arr }}</h2>
-    <p>Total of array: {{ total }}</p>
-    <button @click="pushARandomNumber">Push a random number</button>
   </div>
+  <h1 class="heading" :class="{'is-active': active}">{{ msg }}</h1>
+  <!-- <h1 
+    class="heading" 
+    :class="[active ? 'is-active' : 'no-active']"
+    :style="{'background-color': 'red', border: '5px solid #000'}"
+  >
+      {{ msg }}
+  </h1> -->
+  <button @click="active = !active">Active</button>
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+  <p class="msg" :class="h1Class">Message: {{ msg }}</p>
+  <!-- Có thể bỏ v-bind và bind id-->
+  <a :href="url">{{ url }}</a>
+  <h1>{{ msg.split('').reverse().join('') }}</h1>
+  <input type="text" :value="`xinchao-{}`">
+  <h2 :[classOrId] = "classOrId">{{ true }}</h2>
+  <form @submit.prevent = "onSubmit">
+    <input type="number" v-model.number="msg">
+    <input type="submit">
+  </form>
+  <!-- v-bind cho attribute - : viết tắt -->
+  <!-- v-on: bind cho sự kiện event - @ viết tắt 
+  v-model là -->
+
+
+  <button @click="increase">Increase</button>
+  <p>Count is Even: {{ isEven }}</p>
+  <h2>Count: {{ count }}</h2>
+
+  <p>Dark Mode: {{ isDarkMode }}</p>
+  <button @click="isDarkMode = !isDarkMode">{{ isDarkMode ? "Dark" : "Light" }}</button>
+
+  <h2>{{ user.name }}</h2>
+  <h3>{{ user.age }}</h3>
+  <button @click="increaseAge">Increase age</button>
+  <h1>{{ arr }}</h1>
+  <h1>Sum: {{ total }}</h1>
+    <button @click="addArr">Add arr</button>
+    <button @click="removeArr">Remove arr</button>
+
 </template>
 
 <style scoped>
@@ -118,7 +123,12 @@ const styleObj = reactive({ "font-size": "12px", color: "orange" });
   filter: drop-shadow(0 0 2em #42b883aa);
 }
 
-.h1.is-active {
-  color: pink;
+h1 {
+  transition: all .3s;
+}
+
+h1.is-active {
+    font-size: 64px;
+    transform: translateX(50px);
 }
 </style>
