c477ff95f60eec3e0ddabba0403ed45d85ee0beb
= <trandanhkhang482004@gamil.com>
2024-03-31 21:50:25 +0700

    Update lesson03 exercise

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+  import WeatherWidget from './components/WeatherWidget.vue'; 
+</script>
+
+<template>
+  <WeatherWidget></WeatherWidget>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
